b46eaa17b708b5ea24af21548baf4127cb18560f
rodrigo juaniquina poka <juaniquinapokar@gmail.com>
2026-02-21 18:23:51 -0400

    feat : add document organization logic

--- semana02/organize-files.sh
+++ semana02/organize-files.sh
@@ -13,3 +13,9 @@ mkdir -p organized/{documents,images,scripts,config,logs,temp}
 
 
 echo " Directorios creados "
+# Organizar documentos
+echo "[2/4] Organizando documentos..."
+mv *.txt organized/documents/ 2 >/dev/null || true
+mv *.md organized/documents/ 2 >/dev/null || true
+mv *.doc organized/documents/ 2 >/dev/null || true
+echo "   Documentos organizados"
