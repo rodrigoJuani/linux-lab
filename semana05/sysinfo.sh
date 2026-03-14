722e08609ce1f6c0b0ae61be625f429440f7404e
rodrigo juaniquina poka <juaniquinapokar@gmail.com>
2026-03-14 11:32:20 -0400

    feat: add system info section (hostname,kernel,uptime)

--- semana05/sysinfo.sh
+++ semana05/sysinfo.sh
@@ -60,3 +60,22 @@ echo "$SEPARADOR"
 echo ""
 
 
+# === Sección 1: Información general ===
+seccion_general() {
+echo "[INFORMACION DEL SISTEMA]"
+echo "$SEPARADOR_SEC"
+
+printf "%-18s %s\n" "Hostname:" "$(hostname)"
+printf "%-18s %s\n" "Usuario:" "$USER"
+printf "%-18s %s\n" "Sistema:" "$(uname -s)"
+printf "%-18s %s\n" "Kernel:" "$(uname -r)"
+printf "%-18s %s\n" "Arquitectura:" "$(uname -m)"
+printf "%-18s %s\n" "Fecha / Hora:" "$(date '+%d/%m/%Y %H:%M:%S')"
+printf "%-18s %s\n" "Encendido:" "$(uptime -p)"
+
+echo ""
+}
+# === Ejecutar según el modo ===
+if [ "$MODO" = "all" ]; then
+    seccion_general
+fi
