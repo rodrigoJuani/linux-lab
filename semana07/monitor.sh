60085b93a39f427556efbb9cddf08f3291aa4412
rodrigo juaniquina poka <juaniquinapokar@gmail.com>
2026-03-27 18:18:35 -0400

    feat: add measurement functions and logger with tee

--- semana07/monitor.sh
+++ semana07/monitor.sh
@@ -27,6 +27,34 @@ uso() {
     exit 2
 }
 
+# Retorna el porcentaje de uso de la particion raiz
+uso_disco() {
+    df / | awk 'NR==2 { gsub(/%/,"",$5); print $5 }'
+}
+
+# Retorna el porcentaje de RAM usada
+uso_ram() {
+    free | awk '/^Mem:/ {
+        printf "%.0f", ($3 / $2) * 100
+    }'
+}
+
+# Retorna el load average del ultimo minuto
+carga_cpu() {
+    uptime | awk -F 'load average: ' '{ print $2 }' \
+    | awk '{ gsub(/,/,"",$1); print $1 }'
+}
+
+# Escribe al log y a pantalla con marca de tiempo
+registrar() {
+    local nivel="$1"
+    local mensaje="$2"
+    local ts
+    ts=$(date '+%Y-%m-%d %H:%M:%S')
+    printf "[%s] [%-7s] %s\n" \
+        "$ts" "$nivel" "$mensaje" | tee -a "$LOGFILE"
+}
+
 # Procesar argumentos con while
 while [ $# -gt 0 ]; do
     case "$1" in
